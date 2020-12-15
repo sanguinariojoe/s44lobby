731c2129143c7bc25f1b3224f7210992fdad64bf
Jose Luis Cercos-Pita <jlcercos@gmail.com>
2020-12-15 17:52:42 +0100

    Disabled some useless gadgets

---
+++ LuaRules/Gadgets/ai_craig.lua
@@ -0,0 +1,11 @@
+function gadget:GetInfo()
+    return {
+        name     = "C.R.A.I.G.",
+        desc     = 'Phony gadget to disable the original one without affecting real game options',
+        author   = 'Jose Luis Cercos-Pita',
+        date     = '2019-05-27',
+        license  = 'GNU GPL v2',
+        layer    = 51,
+        enabled  = true,
+    }
+end
